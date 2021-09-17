aab02fb0695cad42b060b63566ede092be32a0b2
Doğukan Öksüz <me@dogukan.dev>
2021-09-17 15:20:08 +0300

    User list feature added

---
+++ cmd/list_users.go
@@ -0,0 +1,52 @@
+package cmd
+
+import (
+	"log"
+	"os"
+
+	"github.com/limanmys/limanctl/helpers"
+	"github.com/limanmys/limanctl/models"
+	"github.com/olekukonko/tablewriter"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(listUsersCmd)
+}
+
+func listUsers() {
+	db := helpers.DB
+
+	var users []models.User
+	if result := db.Find(&users); result.Error != nil {
+		log.Fatal("User not found on Liman")
+	}
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"User Type", "Name", "E-mail", "Last Login At", "Last Login IP", "Auth Type"})
+
+	for _, v := range users {
+		var userType string
+		if v.Status == 1 {
+			userType = "Admin"
+		} else {
+			userType = "User"
+		}
+		table.Append([]string{
+			userType, v.Name, v.Email, v.LastLoginAt, v.LastLoginIP, v.AuthType,
+		})
+	}
+
+	table.Render()
+}
+
+var (
+	listUsersCmd = &cobra.Command{
+		Use:   "users",
+		Short: "List Liman users",
+		Long:  "You can list all Liman users",
+		Run: func(cmd *cobra.Command, args []string) {
+			listUsers()
+		},
+	}
+)
